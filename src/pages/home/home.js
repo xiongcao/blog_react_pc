57ef1a5b81485e3ebdfa6cfa893061ae3ef64386
xiongchao <1359209000@qq.com>
2019-07-24 10:40:53 +0800

    添加删除文件

---
+++ src/pages/home/home.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+class Home extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2>#Home</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default Home
---
+++ src/pages/test/test3.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+class Test3 extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h2>#Test3</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default Test3
